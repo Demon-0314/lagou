a48d044a560a0ae471edcc49937327f8812cda20
Demon_wang <582859604@qq.com>
2020-11-25 01:55:55 +0800

    使用mybtis完成入门案例

---
+++ mybatis_quickStarter/src/main/java/com/lagou/test/MybatisTest.java
@@ -0,0 +1,35 @@
+package com.lagou.test;
+
+import com.lagou.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @ClassName MybatisTest
+ * @Description TODO
+ * @Author demon
+ * @Date 2020/11/25 1:47
+ * @Version 1.0
+ */
+public class MybatisTest {
+    @Test
+    public void test1() throws IOException {
+        InputStream inputStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+        List<User> users = sqlSession.selectList("user.findAll");
+        for (User user : users) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+
+    }
+}
