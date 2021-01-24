cd9e6b1738ccc6293eaa6a41d9cb765265b2c387
Demon_wang <582859604@qq.com>
2021-01-24 22:35:45 +0800

    提交

--- IPersistence/sql/user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `user` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
-INSERT INTO `<table_name>` VALUES (1, '黄忠');
-INSERT INTO `<table_name>` VALUES (2, '关羽');
